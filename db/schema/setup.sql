14c64961debf285ae4ab370fbf6198016c0098f6
fantasticlife <michael@smethur.st:wq>
2023-10-23 21:16:52 +0100

    added publisher table

--- db/schema/setup.sql
+++ db/schema/setup.sql
@@ -1,5 +1,12 @@
 drop table if exists articles;
+drop table if exists publishers;
 
+
+create table publishers (
+	id serial,
+	name varchar(500) not null,
+	primary key (id)
+);
 
 create table articles (
 	id serial,
@@ -10,5 +17,7 @@ create table articles (
 	guid varchar(255) not null,
 	is_posted_to_bluesky boolean default false,
 	is_posted_to_mastodon boolean default false,
+	publisher_id int not null,
+	constraint fk_publisher foreign key (publisher_id) references publishers(id),
 	primary key (id)
 );
