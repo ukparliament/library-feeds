8327125328ab873cef6e30445c76091472d7e4e4
fantasticlife <michael@smethur.st:wq>
2023-10-23 21:19:30 +0100

    added publisher name to query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
   
   def index
-    @articles = Article.all.order( 'published_at desc' )
+    @articles = Article.find_by_sql(
+      "
+        SELECT a.*, p.name AS publisher_name
+        FROM articles a, publishers p
+        WHERE a.publisher_id = p.id
+        ORDER BY a.published_at DESC
+      "
+    )
   end
 end
