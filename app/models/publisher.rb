b3ccc5b794d0a2669da00e3322e038b4a0692335
fantasticlife <michael@smethur.st:wq>
2023-10-24 08:28:16 +0100

    new articles method

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,2 +1,14 @@
 class Publisher < ApplicationRecord
+  
+  def articles
+    Article.find_by_sql(
+        "
+          SELECT a.*, p.name AS publisher_name
+          FROM articles a, publishers p
+          WHERE a.publisher_id = p.id
+          AND p.id = #{self.id}
+          ORDER BY a.published_at DESC
+        "
+      )
+  end
 end
