d24ab94d19cd4a5733ed3e56883c732c256726f5
fantasticlife <michael@smethur.st>
2025-08-17 11:18:01 +0100

    added library design. added route for publications

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  mount LibraryDesign::Engine => "/library_design"
   
   get '/' => 'home#index', as: :root
   get '/library-feeds' => 'home#index', as: :home
+  
+  get '/library-feeds/publications' => 'publication#index', as: :publication_list
+  
+  
+  
+  
   
   get '/library-feeds/publishers' => 'publisher#index', as: :publisher_list
   get '/library-feeds/publishers/:publisher' => 'publisher#show', as: :publisher_show
