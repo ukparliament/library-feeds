7ec1b8ca408e249641309cd619d76d8783662451
fantasticlife <michael@smethur.st>
2025-08-17 08:44:49 +0100

    stripped out home controller to just be home page. need to add test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,5 @@
 class HomeController < ApplicationController
   
   def index
-    @page_title = 'All articles'
-    @articles = Article.find_by_sql(
-      "
-        SELECT a.*, p.name AS publisher_name
-        FROM articles a, publishers p
-        WHERE a.publisher_id = p.id
-        ORDER BY a.published_at DESC
-      "
-    )
   end
 end
